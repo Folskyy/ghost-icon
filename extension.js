07af8ab54cf64e462d199cc07505097960280fd8
Gabriel Vieira <gabrielvieirahc@gmail.com>
2025-12-14 18:19:27 -0300

    Add GhostIcon extension with timer and icon display functionality

---
+++ extension.js
@@ -0,0 +1,133 @@
+import St from 'gi://St';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import Clutter from 'gi://Clutter';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+
+const ICON_SIZE = 64;
+const MARGIN = 20;
+
+export default class GhostIconExtension extends Extension {
+    enable() {
+        log('GhostIcon: enable');
+
+        // Carrega as configurações
+        this._settings = this.getSettings();
+
+        const iconFile = Gio.File.new_for_path(`${this.path}/icon.png`);
+        const gicon = new Gio.FileIcon({ file: iconFile });
+
+        this._icon = new St.Icon({
+            gicon: gicon,
+            icon_size: ICON_SIZE,
+            reactive: false,
+            opacity: 0,
+        });
+
+        // Aparece em tela cheia
+        Main.layoutManager.addChrome(this._icon, { trackFullscreen: true });
+        
+        this._updatePosition();
+        this._icon.hide();
+
+        this._monitorsChangedId = Main.layoutManager.connect(
+            'monitors-changed', 
+            () => this._updatePosition()
+        );
+
+        // Observa mudanças nas configurações para reiniciar o timer se necessário
+        this._settingsChangedId = this._settings.connect('changed::interval', () => {
+            log('GhostIcon: Intervalo alterado pelo usuário, reiniciando timer...');
+            this._startTimer();
+        });
+
+        // Inicia o loop
+        this._startTimer();
+    }
+
+    disable() {
+        log('GhostIcon: disable');
+
+        this._stopTimer();
+
+        if (this._settingsChangedId) {
+            this._settings.disconnect(this._settingsChangedId);
+            this._settingsChangedId = null;
+        }
+        
+        this._settings = null;
+
+        if (this._monitorsChangedId) {
+            Main.layoutManager.disconnect(this._monitorsChangedId);
+            this._monitorsChangedId = null;
+        }
+
+        if (this._icon) {
+            Main.layoutManager.removeChrome(this._icon);
+            this._icon.destroy();
+            this._icon = null;
+        }
+    }
+
+    _startTimer() {
+        // Remove timer anterior se existir
+        this._stopTimer();
+
+        // Lê o valor atual das configurações
+        const interval = this._settings.get_int('interval');
+
+        this._intervalId = GLib.timeout_add_seconds(
+            GLib.PRIORITY_DEFAULT,
+            interval,
+            () => {
+                this._showIcon();
+                return GLib.SOURCE_CONTINUE;
+            }
+        );
+    }
+
+    _stopTimer() {
+        if (this._intervalId) {
+            GLib.Source.remove(this._intervalId);
+            this._intervalId = null;
+        }
+        if (this._hideTimeoutId) {
+            GLib.Source.remove(this._hideTimeoutId);
+            this._hideTimeoutId = null;
+        }
+    }
+
+    _showIcon() {
+        if (!this._icon) return;
+
+        this._icon.remove_all_transitions();
+        this._updatePosition();
+        this._icon.show();
+        this._icon.opacity = 255;
+
+        // Lê a duração configurada neste momento
+        const showDuration = this._settings.get_int('show-duration');
+
+        this._icon.ease({
+            opacity: 0,
+            duration: showDuration * 1000,
+            mode: Clutter.AnimationMode.EASE_IN_QUAD,
+            onComplete: () => {
+                if (this._icon) this._icon.hide();
+            }
+        });
+    }
+
+    _updatePosition() {
+        if (!this._icon) return;
+
+        const monitorIdx = global.display.get_primary_monitor();
+        const monitor = global.display.get_monitor_geometry(monitorIdx);
+
+        this._icon.set_position(
+            monitor.x + monitor.width - ICON_SIZE - MARGIN,
+            monitor.y + monitor.height - ICON_SIZE - MARGIN
+        );
+    }
+}
